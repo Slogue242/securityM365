ddd71428a5e2a93c8c60a96c9d7aa5acf7a04574
Shane <44891132+Slogue242@users.noreply.github.com>
2022-07-27 19:09:32 +1200

    Add files via upload

---
+++ IntuneAutoConfig/CreateGroups.ps1
@@ -0,0 +1,3 @@
+﻿Connect-AzureAD
+New-AzureADGroup -DisplayName "Default_Intune" -MailEnabled $false -SecurityEnabled $true -MailNickName "NotSet"
+New-AzureADGroup -DisplayName "Test_Intune" -MailEnabled $false -SecurityEnabled $true -MailNickName "NotSet"
